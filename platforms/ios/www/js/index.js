de986ed9cbcea3b5e3a619c408a3e86da4179806
Brian Lukoff <brian.lukoff@gmail.com>
2014-03-08 20:49:08 -0800

    Add jQuery mobile icons, and incorporate (very) basic opportunity searching.

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,3 +1,7 @@
+// init client
+var odc = new OneDegreeClient(ODRS['host'], ODRS['version'], ODRS['apiKey']);
+
+// create a status bar
 $(document).on('deviceready', function() {
   StatusBar.overlaysWebView(false);
   StatusBar.backgroundColorByName('gray');
@@ -5,34 +9,62 @@ $(document).on('deviceready', function() {
 
 // tag search on home page
 var tags = [
-  ['health', ['alcohol', 'dental', 'drugs', 'food', 'medical', 'mental-health', 'therapy']],
-  ['housing', ['affordable-housing', 'emergency-shelter', 'home-buying', 'rent-assistance', 'shelter', 'tenants-rights', 'foreclosure']],
-  ['learning', ['after-school', 'children', 'scholarships', 'school', 'skills-training', 'tutoring', 'summer', 'college']],
-  ['money', ['career', 'cash-assistance', 'clothing', 'finances', 'finding-work', 'small-business', 'taxes', 'loans']],
-  ['family', ['child-care', 'domestic-violence', 'immigration', 'lgbt', 'youth']]
-]
+  ['health', 'plus-square', ['alcohol', 'dental', 'drugs', 'food', 'medical', 'mental-health', 'therapy']],
+  ['housing', 'home', ['affordable-housing', 'emergency-shelter', 'home-buying', 'rent-assistance', 'shelter', 'tenants-rights', 'foreclosure']],
+  ['learning', 'book', ['after-school', 'children', 'scholarships', 'school', 'skills-training', 'tutoring', 'summer', 'college']],
+  ['money', 'usd', ['career', 'cash-assistance', 'clothing', 'finances', 'finding-work', 'small-business', 'taxes', 'loans']],
+  ['family', 'group', ['child-care', 'domestic-violence', 'immigration', 'lgbt', 'youth']]
+];
 for (var i = 0; i < tags.length; i++) {
   var category = tags[i][0];
-  var tagList = tags[i][1];
-  $('#tag-list')
-    .append($('<input type="radio" name="tag" id="tag-' + category + '" value="on">'))
-    .append($('<label for="tag-' + category + '">' + _.str.humanize(category) + '</label>'));
+  var icon = tags[i][1];
+  var tagList = tags[i][2];
+  $('#tag-list').append($('<a href="#" data-role="button" data-icon="' + icon + '" class="tag" id="tag-' + category + '" value="on" />').html( _.str.humanize(category)))
 
-  var fieldset = $('<fieldset data-role="controlgroup" data-type="horizontal" data-mini="true" id="tag-' + category + '-list" />');
+  var fieldset = $('<div data-role="controlgroup" data-type="horizontal" data-mini="true" class="tag-list" id="tag-' + category + '-list" />');
   for (var j = 0; j < tagList.length; j++) {
-    fieldset.append($('<input type="radio" data-tag-search="true" id="tag-' + tagList[j] + '" value="on">'));
-    fieldset.append($('<label for="tag-' + tagList[j] + '">' + _.str.humanize(tagList[j]) + '</label>'));
+    fieldset.append($('<a href="#" class="ui-btn" id="tag-' + tagList[j] + '" />').html(_.str.humanize(tagList[j])));
   }
   $('#tag-lists').append(fieldset);
 }
 
 $('[data-role=controlgroup][id!=tag-list]').hide();
-$('[name=tag]').on('click', function() { //
+$('.tag').on('click', function() {
   $('[data-role=controlgroup][id!=tag-list]').fadeOut(200);
   $('#' + $(this).attr('id') + '-list').delay(201).fadeIn(200);
 });
-$('[data-tag-search=true]').on('click', function() {
-  var tag = $(this).attr('id').substring(4);
-  $('#temp').html(tag);
-  $(':mobile-pagecontainer').pagecontainer('change', '#opportunities');
+$('.tag-list a').on('click', function() {
+  opportunitySearch($(this).attr('id').substring(4));
 });
+
+$('#home_search').on('submit', function() {
+  opportunitySearch($('#search').val());
+  return false;
+})
+
+function opportunitySearch(query) {
+  $.mobile.loading('show', {
+    text: "Searching opportunities",
+    textVisible: true
+  });
+  
+  var opps = odc.findOpportunities(query, function(data) {
+    $.each(data['opportunities'], function(index, opp) {
+      $('#temp').append(opp['title']);
+    });
+  });
+
+  $.mobile.loading('hide');
+
+  $('#temp').html(query);
+  $.mobile.pageContainer.pagecontainer('change', '#opportunities');
+}
+
+// $(document).on('ready', function() {
+//   var c = new OneDegreeClient(ODRS['host'], ODRS['version'], ODRS['apiKey']);
+//   var opps = c.findOpportunities('jobs', function(data) {
+//     $.each(data['opportunities'], function(index, opp) {
+//       $('#test_area').append(opp['title']);
+//     });
+//   });
+// });
