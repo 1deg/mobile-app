9013365353b008b0c5d01aa7efec3a07354ae968
Eric Lukoff <eric@1deg.org>
2014-03-09 11:59:09 -0700

    Pull out the tags object into its own file.

---
+++ www/js/tags.js
@@ -0,0 +1,7 @@
+var tags = [
+  ['health', 'plus-square', ['alcohol', 'dental', 'drugs', 'food', 'medical', 'mental-health', 'therapy']],
+  ['housing', 'home', ['affordable-housing', 'emergency-shelter', 'home-buying', 'rent-assistance', 'shelter', 'tenants-rights', 'foreclosure']],
+  ['learning', 'book', ['after-school', 'children', 'scholarships', 'school', 'skills-training', 'tutoring', 'summer', 'college']],
+  ['money', 'usd', ['career', 'cash-assistance', 'clothing', 'finances', 'finding-work', 'small-business', 'taxes', 'loans']],
+  ['family', 'group', ['child-care', 'domestic-violence', 'immigration', 'lgbt', 'youth']]
+];
