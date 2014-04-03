bf842b821fed5c09db06c8da1199a94d3275da48
Eric Lukoff <eric@1deg.org>
2014-04-03 00:50:45 -0700

    Move config vars to just the js folder.

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+var ODRS = {
+  apiKey: "500e3d5089560131c7060614a1d2fcd4",
+  version: "v1",
+  host: "https://data.1deg.org"
+};
+
+var GOOGLE = {
+  apiKey: 'AIzaSyCOe7FGcDiGKYELpXKC3UEnLUkNf0jOysk'
+};
